2a1dab9331cf2649e4e2cdba3f1e63bc1af64b83
Rotem Kincaid <rotema23@gmail.com>
2024-10-18 08:51:15 -0700

    DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,180 @@
 import './App.css'
-import { useContext, useEffect, useRef, useState } from 'react'
-import { AppContext } from './context/AppContext'
+import { useEffect, useRef, useState } from 'react'
 import InputMask from 'react-input-mask'
 import CountryFlag from 'react-country-flag'
+import apiClient from './api/apiClient'
 
 export type CountryData = {
-  id: number;
-  name: string;
-  calling_code: string;
-  phone_length: number;
-  country_code: string;
-};
+  id: number
+  name: string
+  calling_code: string
+  phone_length: number
+  country_code: string 
+}
+
+export type TwoFactorPayload = {
+  country_id: number
+  phone_number: number | undefined
+}
 
 function App() {
-  const { countryData } = useContext(AppContext)
-  const countryArray: CountryData[] = (Object.keys(countryData) as Array<keyof typeof countryData>).map(key => {
-    return { ...countryData[key], country_code: key }
-  });
-
   const [selectedCountry, setSelectedCountry] = useState<CountryData | null>(null)
   const [showList, setShowList] = useState<boolean>(false)
   const [searchString, setSearchString] = useState<string>('')
+  const [phoneNumber, setPhoneNumber] = useState<string>('')
+  const [phoneMask, setPhoneMask] = useState<string>('(999) 999-9999')
+  const [phonePlaceHolder, setPhonePlaceHolder] = useState<string>('(000) 000-0000')
+  const [countries, setCountries] = useState<CountryData[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+
   const dropdownRef = useRef<HTMLDivElement>(null)
+
+  // Convert the response to an array of objects I can use
+  const convertCountriesObject = (countriesObj: Record<string, CountryData>): CountryData[] => {
+    return Object.keys(countriesObj).map((key) => ({
+      ...countriesObj[key],
+      country_code: key, 
+    }))
+  }
+
+  const fetchCountries = async () => {
+    setLoading(true)
+    try {
+      const response = await apiClient.get('/challenges/countries')
+      const converted = convertCountriesObject(response.data)
+      setCountries(converted)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  // when phone number is submitted, send this request to the api
+  const reqTwoFactor = async (obj: TwoFactorPayload) => {
+    setLoading(true)
+    try {
+      const response = await apiClient.post('/challenges/two_factor_auth', obj)
+
+      if (response.status === 200) console.log("YAY!")
+    } catch (error) {
+        throw new Error()
+    } finally {
+      setLoading(false) // Set loading to false after the request is done
+    }
+  }
 
   const handleSearch = (value: string) => {
     setSearchString(value)
-  };
+  }
 
   const handleSelect = (country: CountryData) => {
-    setSelectedCountry(country) // Set selected country 
+    setSelectedCountry(country)
     setShowList(false)
-  };
+  }
 
   const toggleList = (e: any) => {
-    e.preventDefault()
-    if(searchString.length) setSearchString('')
+    if (searchString.length) setSearchString('')
+    setShowList(!showList)
+  }
 
-    setShowList(!showList) // Toggle dropdown
-  };
-
-  const filteredCountries = countryArray.filter(country =>
+  const filteredCountries = countries.filter((country) =>
     country.name.toLowerCase().includes(searchString.toLowerCase())
-  );
+  )
 
   const handleClickOutside = (event: MouseEvent) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-      setShowList(false); // Close dropdown if clicked outside
+      setShowList(false)
     }
-  };
+  }
 
-  // Dynamically changing the phone mask based on selected country
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+
+    if (!selectedCountry) {
+      console.error('Country not selected')
+      return
+    }
+
+    const fullPhoneNumber = `${selectedCountry?.calling_code} ${phoneNumber}`
+    const phoneAsInteger = parseInt(fullPhoneNumber.replace(/\D/g, ''), 10)
+
+  
+    const payload: TwoFactorPayload = {
+       phone_number: phoneAsInteger, 
+       country_id: selectedCountry?.id 
+    }
+    reqTwoFactor(payload)
+  }
+
+  // handle phone mask based on length
   const getPhoneMask = (length: number) => {
-    if (length === 10) return '(999) 999-9999';
-    else if (length === 11) return '999 999 99999'
-    else if (length === 12) return '9999 999 9999'
-    else if (length === 13) return '99999 999 9999'
-    return '999999999999999' // Default mask with up to 15 digits
-  };
+    const parsedLength = Number(length)
+    
+    if (parsedLength === 10) return '(999) 999-9999'
+    else if (parsedLength === 11) return '999 999 99999'
+    else if (parsedLength === 12) return '9999 999 9999'
+    else if (parsedLength === 13) return '99999 999 9999'
+    else return '(999) 999-9999'
+  }
 
+  const getPhonePlaceholder = (length: number) => {
+    const parsedLength = Number(length)
 
+    if (parsedLength === 10) return '(000) 000-0000'
+    else if (parsedLength === 11) return '000 000 00000'
+    else if (parsedLength === 12) return '0000 000 0000'
+    else if (parsedLength === 13) return '00000 000 0000'
+    else return '0'.repeat(parsedLength)
+  }
+
+  // when clicked outside of the list, close it
   useEffect(() => {
+    if (!selectedCountry) return
+
+    setPhoneMask(getPhoneMask(selectedCountry.phone_length))
+    setPhonePlaceHolder(getPhonePlaceholder(selectedCountry.phone_length))
     document.addEventListener('mousedown', handleClickOutside)
     return () => {
       document.removeEventListener('mousedown', handleClickOutside)
-    };
+    }
   }, [selectedCountry])
+
+  useEffect(() => {
+    fetchCountries()
+  },[])
 
   return (
     <div className="bg-violet-400 w-[100vw] h-[100vh]">
-      <form onSubmit={(e) => e.preventDefault()} className="min-h-[80vh] flex items-center">
+      <form onSubmit={(e) => handleSubmit(e)} className="min-h-[80vh] flex items-center">
         <div className="relative flex flex-col items-center gap-3 m-auto p-8 min-w-[340px] sm:min-w-96 border bg-slate-400 rounded-xl text-zinc-600 text-sm shadow-lg">
-          
-          {/* Button that triggers dropdown */}
-          <div className="flex justify-between w-[267px]">
-            <button
-              onClick={(e) => toggleList(e)}
-              type="button"
-              className="w-16 h-8 rounded-md bg-white pl-2"
-            >
+        {loading && <div className="absolute top-0 left-0 right-0 text-white text-center">Loading...</div>} {/* Loading indicator */}
+
+          <div className="flex justify-between w-[275px]">
+            <button onClick={(e) => toggleList(e)} type="button" className="w-18 h-8 rounded-md bg-white px-1">
               <div className="flex items-center justify-around">
-              <CountryFlag
-                  countryCode={selectedCountry ? selectedCountry.country_code : countryArray[0].country_code}
+                <CountryFlag
+                  countryCode={selectedCountry ? selectedCountry.country_code : countries[0]?.country_code}
                   svg
-                  style={{ width: '30px', height: '20px'}}
-                />                
-                <div>{selectedCountry ? selectedCountry.calling_code : countryArray[0].calling_code}</div>
+                  style={{ width: '30px', height: '20px' }}
+                />
+                <div>{selectedCountry ? selectedCountry.calling_code : countries[0]?.calling_code}</div>
               </div>
             </button>
             <InputMask
-              mask={getPhoneMask(selectedCountry? selectedCountry.phone_length : 0)}
+              mask={phoneMask}
               className="w-[200px] pl-2 rounded-md text-base"
               type="text"
               name="phone"
-              placeholder="(xxx) xxx-xxxx"
+              placeholder={phonePlaceHolder}
+              onChange={e => setPhoneNumber(e.target.value)}
             />
           </div>
 
           {/* Dropdown list */}
           <div
-            ref={dropdownRef} // Attach ref here
-            className={`absolute top-16 mt-2 sm:left-14 w-[267px] border rounded-xl bg-white shadow-lg transition-all duration-300 ease-in-out transform origin-top-right ${
+            ref={dropdownRef}
+            className={`absolute top-16 mt-2 sm:left-13 w-[275px] border rounded-xl bg-white shadow-lg transition-all duration-300 ease-in-out transform origin-top-right ${
               showList ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-5 pointer-events-none'
             }`}
           >
@@ -112,6 +187,7 @@ function App() {
             <div className="max-h-[100px] w-[96%] mx-2 my-2 overflow-y-scroll border border-slate-500 rounded-md flex flex-col items-start pl-2 py-2">
               {filteredCountries.map((country, index) => (
                 <button
+                  type="button"
                   className="w-full flex items-center justify-start py-1"
                   key={index}
                   onClick={() => handleSelect(country)}
@@ -120,20 +196,20 @@ function App() {
                     countryCode={country.country_code}
                     svg
                     style={{ width: '30px', height: '20px' }}
-                  />                  
-                  <div>{country.name} {country.calling_code}</div>
+                  />
+                  <div className='pl-2'>{country.name} {country.calling_code}</div>
                 </button>
               ))}
             </div>
           </div>
 
-          <button className="min-w-[267px] border border-white bg-slate-100 text-slate-800 font-bold px-8 py-2 rounded-md hover:bg-violet-900 hover:text-white transition-all">
+          <button className="min-w-[275px] border border-white bg-slate-100 text-slate-800 font-bold px-8 py-2 rounded-md hover:bg-violet-900 hover:text-white transition-all" type="submit">
             Submit
           </button>
         </div>
       </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,32 +1,66 @@
 import axios from 'axios'
 
+// Initialize token
 let token = null
+let isFetchingToken = false // Flag to prevent multiple token requests
 
-// token request 
+// Function to request authentication token
 const getAuthToken = async () => {
     try {
-        const response = await axios.post('https://sandbox-api.softpoint.io/interface/v1/access_token', 
-        {
-            apiKey: '?????'
+        const response = await apiClient.post('/access_token', null, {
+            headers: {
+                'Api-Key': process.env.REACT_APP_API_KEY,
+            },
+            params: {
+                corporate_id: 10
+            },
         })
 
-        token = response.data.token
-
+        token = response.data.access_token
         return token
     } catch (error) {
         console.error('Error fetching auth token:', error)
-        throw error
+        throw new Error(error.message) // Rethrow the error with the message
     }
 }
 
-// axios instance
+// Create Axios instance
 const apiClient = axios.create({
-    baseUrl: 'https://sandbox-api.softpoint.io/interface/v1',
-    timeout: 10000, // 10 seconds timeout
+    baseURL: 'https://sandbox-api.softpoint.io/interface/v1',
+    timeout: 10000,
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
     },
 })
 
+// Add a request interceptor
+apiClient.interceptors.request.use(
+    async (config) => {
+        // If no token is available, request a new one
+        if (!token) {
+            if (!isFetchingToken) {
+                isFetchingToken = true // Set the flag to prevent duplicate requests
+                try {
+                    token = await getAuthToken() // Get a new token
+                } catch (error) {
+                    // Handle the error appropriately
+                    console.error('Failed to fetch token:', error)
+                    return Promise.reject(error) // Reject the request
+                } finally {
+                    isFetchingToken = false // Reset the flag
+                }
+            } else {
+                // Optional handling (the case where a token fetch is already in progress)
+            }
+        }
 
+        config.headers.Authorization = `Bearer ${token}`
 
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+export default apiClient
